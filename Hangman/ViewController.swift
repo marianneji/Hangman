fe12f50f3c215b65b4d2e1de54be1df9399d15b9
marianneji <marianneji@gmail.com>
2019-07-26 17:43:37 +0200

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Graphic Influence on 26/07/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
